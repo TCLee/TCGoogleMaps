97226a175f8006529e84eb1f8d7affcb95f018cd
Lee Tze Cheun <leetzecheun@gmail.com>
2013-09-06 14:22:24 +0800

    Consolidate imports for Google Places API library classes into TCGooglePlaces.h

---
+++ TCGoogleMaps/Lib/TCGoogleMapsLib/Places/TCGooglePlaces.h
@@ -0,0 +1,14 @@
+//
+//  TCGooglePlaces.h
+//  TCGoogleMaps
+//
+//  Created by Lee Tze Cheun on 9/6/13.
+//  Copyright (c) 2013 Lee Tze Cheun. All rights reserved.
+//
+
+#ifndef TCGoogleMaps_TCGooglePlaces_h
+#define TCGoogleMaps_TCGooglePlaces_h
+
+
+
+#endif
